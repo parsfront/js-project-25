6f9caffe1ed72ea80cb1dad2ea546d4d01f48513
Ali Bahadoran <alibahadoran.eng@gmail.com>
2022-07-12 20:35:38 +0430

    Javascript mouse project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const button = document.querySelectorAll("a");
+
+button.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+        console.log(y);
+        let span = document.createElement("span");
+        span.style.left = x + "px";
+        span.style.top = y + "px";
+        this.appendChild(span);
+
+        setTimeout(() => {
+            span.remove();
+        }, 1000);
+    });
+});
